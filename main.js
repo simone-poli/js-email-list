d94a354117dc94ec9a398e79479822829b81957c
simone-poli <polismn97@gmail.com>
2025-06-16 16:05:34 +0200

    fix with ul

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
 
+
+
+
+let ul = document.createElement("ul")
+    document.body.appendChild(ul)
 for (let i = 0; i < 10; i++){
     fetch(endpoint)
     .then(response => response.json())
     .then(data =>{
         console.log(data)
-        const output = document.createElement("p")
-        output.innerText= data.response
-        document.body.appendChild(output)
+        const li = document.createElement("li")
+        li.innerText= data.response
+        ul.appendChild(li)
     })
     .catch(error => {
         console.log(error)
